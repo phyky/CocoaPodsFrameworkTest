8059629e529af9bd512f2829ab72c6decd51ed25
phyky <phyky@phyky-2.local>
2018-09-10 16:27:24 +0800

    提交framework

---
+++ lib/PHYFrameworkTest.framework/Headers/PHYFrameworkTest.h
@@ -0,0 +1,19 @@
+//
+//  PHYFrameworkTest.h
+//  PHYFrameworkTest
+//
+//  Created by phyky on 2018/9/10.
+//  Copyright © 2018年 coolding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PHYFrameworkTest.
+FOUNDATION_EXPORT double PHYFrameworkTestVersionNumber;
+
+//! Project version string for PHYFrameworkTest.
+FOUNDATION_EXPORT const unsigned char PHYFrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PHYFrameworkTest/PublicHeader.h>
+
+#import <PHYFrameworkTest/PHYLog.h>
---
+++ lib/PHYFrameworkTest.framework/Headers/PHYLog.h
@@ -0,0 +1,14 @@
+//
+//  PHYLog.h
+//  FrameworkTest
+//
+//  Created by phyky on 2018/9/10.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PHYLog : NSObject
+
++(void)logMessage:(NSString *)message;
+
+@end
